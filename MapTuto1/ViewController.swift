2e5a02c33d43d658031ea4b78fce58cc6d0e2964
Andréa <andreaportillao@gmail.com>
2018-11-18 14:49:10 -0500

    "Setting Visible Area" error
    Last step finished: // set initial location in Honolulu
    let initialLocation = CLLocation(latitude: 21.282778, longitude: -157.829444)
    
    FIX: Initialization of immutable value 'initialLocation' was never used; consider replacing with assignment to '_' or removing it.
    Replace 'let initialLocation' with '_'.

--- MapTuto1/ViewController.swift
+++ MapTuto1/ViewController.swift
@@ -7,12 +7,18 @@
 //
 
 import UIKit
+import MapKit
+
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var mapView: MKMapView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        // set initial location in Honolulu
+        let initialLocation = CLLocation(latitude: 21.282778, longitude: -157.829444)
+
     }
 
 
