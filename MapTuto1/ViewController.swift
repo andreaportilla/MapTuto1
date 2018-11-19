07be2e85040f8cc3c5c9d4bf66e55103fbce9e79
Andréa <andreaportillao@gmail.com>
2018-11-19 13:40:05 -0500

    "Build and run your app and check out all the markers!" NOT WORKING
    Got this warning:
    'flatMap' is deprecated: Please use compactMap(_:) for the case where closure returns an optional value.
    Use 'compactMap(_:)' instead.
    
    Not fixed yet.

--- MapTuto1/ViewController.swift
+++ MapTuto1/ViewController.swift
@@ -12,6 +12,7 @@ import MapKit
 
 class ViewController: UIViewController {
     @IBOutlet weak var mapView: MKMapView!
+    var artworks: [Artwork] = []
     let regionRadius: CLLocationDistance = 1000
     
     override func viewDidLoad() {
@@ -21,12 +22,15 @@ class ViewController: UIViewController {
         let initialLocation = CLLocation(latitude: 21.282778, longitude: -157.829444)
         centerMapOnLocation(location: initialLocation)
         // show artwork on map
-        mapView.delegate = self
-        let artwork = Artwork(title: "King David Kalakaua",
-                              locationName: "Waikiki Gateway Park",
-                              discipline: "Sculpture",
-                              coordinate: CLLocationCoordinate2D(latitude: 21.283921, longitude: -157.831661))
-        mapView.addAnnotation(artwork)
+         mapView.delegate = self
+        //let artwork = Artwork(title: "King David Kalakaua",
+        //                      locationName: "Waikiki Gateway Park",
+        //                      discipline: "Sculpture",
+        //                      coordinate: CLLocationCoordinate2D(latitude: 21.283921, longitude: -157.831661))
+        // mapView.addAnnotation(artwork)
+        loadInitialData()
+        mapView.addAnnotations(artworks)
+
     }
     
     func centerMapOnLocation(location: CLLocation) {
@@ -34,6 +38,27 @@ class ViewController: UIViewController {
                                                   latitudinalMeters: regionRadius, longitudinalMeters: regionRadius)
         mapView.setRegion(coordinateRegion, animated: true)
     }
+    func loadInitialData() {
+        // 1
+        guard let fileName = Bundle.main.path(forResource: "PublicArt", ofType: "json")
+            else { return }
+        let optionalData = try? Data(contentsOf: URL(fileURLWithPath: fileName))
+        
+        guard
+            let data = optionalData,
+            // 2
+            let json = try? JSONSerialization.jsonObject(with: data),
+            // 3
+            let dictionary = json as? [String: Any],
+            // 4
+            let works = dictionary["data"] as? [[Any]]
+            else { return }
+        // 5
+        let validWorks = works.flatMap { Artwork(json: $0) }
+        artworks.append(contentsOf: validWorks)
+    }
+    
+
 }
 
 extension ViewController: MKMapViewDelegate {
